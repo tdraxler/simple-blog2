e8d2f1a8fcdc1fa48d2bc7a85ab14a18484a8f9c
tdraxler <twdrax@gmail.com>
2020-07-06 21:09:46 -0700

    Adds files from react-express-template

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+let app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render("index");
+});
+
+app.listen(3000, () => {
+  console.log("Listening...");
+});
---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const testPhrase = "Hello World!";
+
+ReactDOM.render(
+  <div>{testPhrase}</div>,
+  document.getElementById('app')
+);
